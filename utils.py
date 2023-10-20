e730bd2b842b50f957e8676a6d2488d281985912
Joanna Lim <m210068@e.ntu.edu.sg>
2023-10-20 12:59:49 +0800

    cnn model

---
+++ utils.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+
+def load_dataset(train_dataset, test_dataset):
+    trainX, trainY = train_dataset
+    testX, testY = test_dataset
+
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+
+    # one hot encode target values
+    encoder = OneHotEncoder(sparse=False, categories='auto')
+    trainY = encoder.fit_transform(trainY.reshape(-1, 1))
+    testY = encoder.transform(testY.reshape(-1, 1))
+
+    return trainX, trainY, testX, testY
+
+def prep_pixels(train, test):
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+
+    return train_norm, test_norm
+
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(32 * 14 * 14, 100)
+        self.fc2 = nn.Linear(100, 10)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.softmax(x)
+        return x
+
