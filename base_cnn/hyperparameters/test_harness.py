2f28fffc3e58466d556d987be735e43407fdbaa6
Joanna Lim <m210068@e.ntu.edu.sg>
2023-10-21 15:05:00 +0800

    early stopping

--- base_cnn/hyperparameters/test_harness.py
+++ base_cnn/hyperparameters/test_harness.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from tqdm import tqdm
 
-# order of tuning: batch size, number of epochs, size of fully connected layers, choice of activation function
+# order of tuning: learning rate, size of fully connected layers, batch size, choice of activation function, early stopping
 
 # hyperparameters to tune in this function:
     # batch size: [32, 64, 128] 
@@ -49,15 +49,18 @@ def define_model(fc_layer_size, activation_fn):
     return model
 
 # hyperparameters to tune in this function:
-    # number of epochs: [5, 10, 15, 20]
-from tqdm import tqdm
-
-def train_and_evaluate_model(model, train_loader, test_loader, num_epochs):
+    # learning rate: [0.001, 0.01, 0.1]
+    # early stopping: [3, 4, 5, 6, 7, 8, 9, 10]
+def train_and_evaluate_model(model, train_loader, test_loader, early_stopping, learning_rate):
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+    best_accuracy = 0.0
+    best_model_weights = model.state_dict()
+    patience_counter = 0
 
     total_step = len(train_loader)
-    for epoch in range(num_epochs):
+    for epoch in range(20):
         pbar = tqdm(enumerate(train_loader), total=total_step)
         for i, (images, labels) in pbar:
             optimizer.zero_grad()
@@ -67,28 +70,40 @@ def train_and_evaluate_model(model, train_loader, test_loader, num_epochs):
             optimizer.step()
 
             if (i + 1) % 200 == 0:
-                pbar.set_description(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{total_step}], Loss: {loss.item()}')
+                pbar.set_description(f'Epoch [{epoch + 1}/{20}], Step [{i + 1}/{total_step}], Loss: {loss.item()}')
 
-    model.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        pbar_test = tqdm(test_loader)
-        for images, labels in pbar_test:
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            pbar_test.set_description(f'Accuracy: {100 * correct / total:.2f}%')
-        
-    accuracy = 100 * correct / total
-    print(f'Accuracy of the model on the test images: {accuracy:.2f}%')
-    return accuracy
+        model.eval()
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            pbar_test = tqdm(test_loader)
+            for images, labels in pbar_test:
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                pbar_test.set_description(f'Accuracy: {100 * correct / total:.2f}%')
+            
+            accuracy = 100 * correct / total
+
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_model_weights = model.state_dict()
+                patience_counter = 0
+            
+            else:
+                patience_counter += 1
+                if patience_counter >= early_stopping:
+                    print(f'Early stopping at epoch {epoch + 1}')
+                    break
+
+    model.load_state_dict(best_model_weights)
+    return best_accuracy
 
 
-def run_test_harness(train_dataset, test_dataset, batch_size, fc_layer_size, activation_fn, num_epochs):
+def run_test_harness(train_dataset, test_dataset, batch_size, fc_layer_size, activation_fn, early_stopping, learning_rate):
     train_loader, test_loader = load_dataset(train_dataset, test_dataset, batch_size)
     train_loader, test_loader = prep_pixels(train_loader, test_loader)
     model = define_model(fc_layer_size, activation_fn)
-    accuracy = train_and_evaluate_model(model, train_loader, test_loader, num_epochs)
+    accuracy = train_and_evaluate_model(model, train_loader, test_loader, early_stopping, learning_rate)
     return accuracy
