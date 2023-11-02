e8732cda4c45c6fb14f4d59b49f55cdf06c12e5d
Joanna Lim <m210068@e.ntu.edu.sg>
2023-11-02 09:13:21 +0800

    vit model.py

--- vision_transformer/utils_vit.py
+++ vision_transformer/utils_vit.py
@@ -13,8 +13,6 @@ def get_loader():
     train_dataset = datasets.FashionMNIST(root='../data', train=True, download=True, transform=train_transform)
     test_dataset = datasets.FashionMNIST(root='../data', train=False, download=True, transform=test_transform)
   
-    
-
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                                  batch_size=128,
                                                  shuffle=True,
