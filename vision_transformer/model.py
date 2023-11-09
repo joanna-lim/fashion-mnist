07f99dc91e2f4b4fbf19ac711112e2cd41b6226c
Joanna Lim <m210068@e.ntu.edu.sg>
2023-11-09 17:27:53 +0800

    FINAL mb

--- vision_transformer/model.py
+++ vision_transformer/model.py
@@ -1,20 +1,6 @@
 import torch
 import torch.nn as nn
 
-# B -> Batch Size
-# C -> Number of Input Channels
-# IH -> Image Height
-# IW -> Image Width
-# P -> Patch Size
-# E -> Embedding Dimension
-# S -> Sequence Length = IH/P * IW/P
-# Q -> Query Sequence length
-# K -> Key Sequence length
-# V -> Value Sequence length (same as Key length)
-# H -> Number of heads
-# HE -> Head Embedding Dimension = E/H
-
-
 class EmbedLayer(nn.Module):
     def __init__(self):
         super().__init__()
@@ -23,11 +9,11 @@ class EmbedLayer(nn.Module):
         self.pos_embedding = nn.Parameter(torch.zeros(1, (28 // 4) ** 2 + 1, 96), requires_grad=True)  
 
     def forward(self, x):
-        x = self.conv1(x)  # B C IH IW -> B E IH/P IW/P (Embedding the patches)
-        x = x.reshape([x.shape[0], 96, -1])  # B E IH/P IW/P -> B E S (Flattening the patches)
-        x = x.transpose(1, 2)  # B E S -> B S E 
-        x = torch.cat((torch.repeat_interleave(self.cls_token, x.shape[0], 0), x), dim=1)  # Adding classification token at the start of every sequence
-        x = x + self.pos_embedding  # Adding positional embedding
+        x = self.conv1(x)  
+        x = x.reshape([x.shape[0], 96, -1])  
+        x = x.transpose(1, 2) 
+        x = torch.cat((torch.repeat_interleave(self.cls_token, x.shape[0], 0), x), dim=1)  
+        x = x + self.pos_embedding  
         return x
 
 class SelfAttention(nn.Module):
@@ -44,25 +30,25 @@ class SelfAttention(nn.Module):
     def forward(self, x):
         m, s, e = x.shape
 
-        xq = self.queries(x).reshape(m, s, self.n_attention_heads, self.head_embed_dim)  # B, Q, E -> B, Q, H, HE
-        xq = xq.transpose(1, 2)  # B, Q, H, HE -> B, H, Q, HE
-        xk = self.keys(x).reshape(m, s, self.n_attention_heads, self.head_embed_dim)  # B, K, E -> B, K, H, HE
-        xk = xk.transpose(1, 2)  # B, K, H, HE -> B, H, K, HE
-        xv = self.values(x).reshape(m, s, self.n_attention_heads, self.head_embed_dim)  # B, V, E -> B, V, H, HE
-        xv = xv.transpose(1, 2)  # B, V, H, HE -> B, H, V, HE
+        xq = self.queries(x).reshape(m, s, self.n_attention_heads, self.head_embed_dim)  
+        xq = xq.transpose(1, 2)  
+        xk = self.keys(x).reshape(m, s, self.n_attention_heads, self.head_embed_dim)  
+        xk = xk.transpose(1, 2) 
+        xv = self.values(x).reshape(m, s, self.n_attention_heads, self.head_embed_dim) 
+        xv = xv.transpose(1, 2) 
 
-        xq = xq.reshape([-1, s, self.head_embed_dim])  # B, H, Q, HE -> (BH), Q, HE
-        xk = xk.reshape([-1, s, self.head_embed_dim])  # B, H, K, HE -> (BH), K, HE
-        xv = xv.reshape([-1, s, self.head_embed_dim])  # B, H, V, HE -> (BH), V, HE
+        xq = xq.reshape([-1, s, self.head_embed_dim]) 
+        xk = xk.reshape([-1, s, self.head_embed_dim]) 
+        xv = xv.reshape([-1, s, self.head_embed_dim])  
 
-        xk = xk.transpose(1, 2)  # (BH), K, HE -> (BH), HE, K
-        x_attention = xq.bmm(xk)  # (BH), Q, HE  .  (BH), HE, K -> (BH), Q, K
+        xk = xk.transpose(1, 2)  
+        x_attention = xq.bmm(xk)  
         x_attention = torch.softmax(x_attention, dim=-1)
 
-        x = x_attention.bmm(xv)  # (BH), Q, K . (BH), V, HE -> (BH), Q, HE
-        x = x.reshape([-1, self.n_attention_heads, s, self.head_embed_dim])  # (BH), Q, HE -> B, H, Q, HE
-        x = x.transpose(1, 2)  # B, H, Q, HE -> B, Q, H, HE
-        x = x.reshape(m, s, e)  # B, Q, H, HE -> B, Q, E
+        x = x_attention.bmm(xv)  
+        x = x.reshape([-1, self.n_attention_heads, s, self.head_embed_dim])  
+        x = x.transpose(1, 2)  
+        x = x.reshape(m, s, e)  
         return x
 
 
@@ -77,8 +63,8 @@ class Encoder(nn.Module):
         self.norm2 = nn.LayerNorm(96)
 
     def forward(self, x):
-        x = x + self.attention(self.norm1(x)) # Skip connections
-        x = x + self.fc2(self.activation(self.fc1(self.norm2(x))))  # Skip connections
+        x = x + self.attention(self.norm1(x)) 
+        x = x + self.fc2(self.activation(self.fc1(self.norm2(x))))  
         return x
 
 
@@ -90,7 +76,7 @@ class Classifier(nn.Module):
         self.fc2 = nn.Linear(96, 10)
 
     def forward(self, x):
-        x = x[:, 0, :]  # Get CLS token
+        x = x[:, 0, :]  
         x = self.fc1(x)
         x = self.activation(x)
         x = self.fc2(x)
